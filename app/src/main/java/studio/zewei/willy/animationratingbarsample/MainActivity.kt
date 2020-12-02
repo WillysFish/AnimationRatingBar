420906930f2c8a25f8e59cd4375cf1741059fdc7
willy_yan <willy.yan@peronaad.com>
2020-12-02 18:27:09 +0800

    Implemented the sample.

--- app/src/main/java/studio/zewei/willy/animationratingbarsample/MainActivity.kt
+++ app/src/main/java/studio/zewei/willy/animationratingbarsample/MainActivity.kt
@@ -1,11 +1,21 @@
 package studio.zewei.willy.animationratingbarsample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        goDemoBtn.setOnClickListener {
+            startActivity(Intent(this, DemoActivity::class.java))
+        }
+        restartBtn.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+
     }
 }
